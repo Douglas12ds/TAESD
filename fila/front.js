31539cd51df4940afe87c096c88e66f9c85d71fe
Douglas12ds <43081345+Douglas12ds@users.noreply.github.com>
2019-04-30 09:26:58 -0300

    interface atualizada

--- fila/front.js
+++ fila/front.js
@@ -12,16 +12,18 @@ listarElementos()
 $(document).on('submit', '#adicionar', function (e) {
     e.preventDefault()
     event.stopImmediatePropagation()
-    fila.inserir($('#valor').val())
-    $valor = "<div class='elemento'>" + $('#valor').val() + "</div>"
-    $('.elemento').last().after($valor)
-    $('.elemento').last().hide().fadeIn(500)
-    /*$('.elemento').last().animate({
-        opacity: 0.25,
-        "margin-left": "-50px"
-    }, 500, function () {
 
-    })*/
+    if (fila.inserir($('#valor').val())) {
+        $('#feedback').val("")
+        $valor = "<div class='elemento'>" + $('#valor').val() + "</div>"
+        $('.elemento').last().after($valor)
+        $('.elemento').last().hide().fadeIn(500)
+        $('#valor').val("")
+    } else {
+        $('#feedback').val("")
+        $('#feedback').val("<p>Impossível enfileirar. Fila cheia!</p>")
+        $('#valor').val("")
+    }
 })
 
 $('#remover').on('click', function () {
